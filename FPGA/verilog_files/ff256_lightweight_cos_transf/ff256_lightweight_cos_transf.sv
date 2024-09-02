2c59691d1f5138795ddd8ccd445799f72c47a1f2
vacoutinho <vitor.coutinho@ufrpe.br>
2024-09-02 16:51:53 -0300

    Adding sequential logic to lightweight FFCT. OBS: not compiling with system verilog. TODO: compile project with this modification.

--- FPGA/verilog_files/ff256_lightweight_cos_transf/ff256_lightweight_cos_transf.sv
+++ FPGA/verilog_files/ff256_lightweight_cos_transf/ff256_lightweight_cos_transf.sv
@@ -2,16 +2,18 @@
 Optimized computation of X_out[:] = F256CT[:,:]*x_in[:].
 x_in[:] shall be furnished sequentially in 8 clock cycle in the order x0, x1, x2..., x7.
 */
-`include "./verilog_files/ff256_cosine_transform/ff256_mult_by_const_defines.sv"
+`include "./verilog_files/ff256_mult_by_const/ff256_mult_by_const_defines.sv"
+`include "./verilog_files/ff256_lightweight_cos_transf/ff256_lightweight_cos_transf_defines.sv"
 
 module ff256_lightweight_cos_transf
-/*#(
-
-)*/
+#(
+    parameter logic [7:0] cnst [0:7] =  '{8'h98, 8'h2d, 8'h5a, 8'hb4, 8'h75, 8'hea, 8'hc9, 8'h8f}
+)
 (
     input  [7:0]  x_in,
     input clk,
     input strt_cmpt,
+    input rst,
     output [63:0]  x_out,
     output output_ready
 );
@@ -31,6 +33,8 @@ assign x_out[55:48] = (output_ready?) x_6_o : 8'hz;
 assign x_out[63:56] = (output_ready?) x_7_o : 8'hz;
 
 wire [7:0] beta_1_data_o, beta_2_data_o, beta_3_data_o, beta_4_data_o, beta_6_data_o, beta_8_data_o, beta_9_data_o, beta_12_data_o;
+
+reg [3:0] state, next_state;
 
 // ##################################################
 // INSTANCES
@@ -77,6 +81,127 @@ ff256_mult_by_const_multiplier #(.cnst(BETA_1)) beta_1_inst(
 
 //#################### SEQUENTIAL LOGIC
 
-// TODO!!
-    
-endmodule 
+    // transition and reset
+    always @(posedge clk or posedge rst) begin
+        if (rst)
+            state <= `IDLE;
+        else
+            state <= next_state;
+    end
+
+    // transiction logic
+    always @(*) begin
+        case (state)
+            `IDLE: next_state = strt_cmpt ? `S0 : `IDLE;
+            `S0: next_state = `S1;
+            `S1: next_state = `S2;
+            `S2: next_state = `S3;
+            `S3: next_state = `S4;
+            `S4: next_state = `S5;
+            `S5: next_state = `S6;
+            `S6: next_state = `S7;
+            `S7: next_state = `DONE;
+            `DONE: next_state = strt_cmpt ? `DONE : `IDLE;
+            default: next_state = `IDLE;
+        endcase
+    end
+
+    // computing and output logic
+    always @(state) begin
+        case (state)
+            `IDLE: 
+                output_ready = 1'b0;
+                x_0_o = 8'd0;
+                x_0_1 = 8'd0;
+                x_0_2 = 8'd0;
+                x_0_3 = 8'd0;
+                x_0_4 = 8'd0;
+                x_0_5 = 8'd0;
+                x_0_6 = 8'd0;
+                x_0_7 = 8'd0;
+            `S0:
+                output_ready = 1'b0;
+                x_0_o = x_0_o ^ beta_3_data_o;
+                x_1_o = x_1_o ^ beta_6_data_o;
+                x_2_o = x_2_o ^ beta_1_data_o;
+                x_3_o = x_3_o ^ beta_12_data_o;
+                x_4_o = x_4_o ^ beta_4_data_o;
+                x_5_o = x_5_o ^ beta_2_data_o;
+                x_6_o = x_6_o ^ beta_8_data_o;
+                x_7_o = x_7_o ^ beta_9_data_o;
+            `S1:
+                output_ready = 1'b0;
+                x_0_o = x_0_o ^ beta_6_data_o;
+                x_1_o = x_1_o ^ beta_12_data_o;
+                x_2_o = x_2_o ^ beta_2_data_o;
+                x_3_o = x_3_o ^ beta_9_data_o;
+                x_4_o = x_4_o ^ beta_8_data_o;
+                x_5_o = x_5_o ^ beta_4_data_o;
+                x_6_o = x_6_o ^ beta_1_data_o;
+                x_7_o = x_7_o ^ beta_3_data_o;
+            `S2: 
+                output_ready = 1'b0;
+                x_0_o = x_0_o ^ beta_1_data_o;
+                x_1_o = x_1_o ^ beta_2_data_o;
+                x_2_o = x_2_o ^ beta_9_data_o;
+                x_3_o = x_3_o ^ beta_4_data_o;
+                x_4_o = x_4_o ^ beta_6_data_o;
+                x_5_o = x_5_o ^ beta_3_data_o;
+                x_6_o = x_6_o ^ beta_12_data_o;
+                x_7_o = x_7_o ^ beta_8_data_o;
+            `S3:
+                output_ready = 1'b0;
+                x_0_o = x_0_o ^ beta_12_data_o;
+                x_1_o = x_1_o ^ beta_9_data_o;
+                x_2_o = x_2_o ^ beta_4_data_o;
+                x_3_o = x_3_o ^ beta_3_data_o;
+                x_4_o = x_4_o ^ beta_1_data_o;
+                x_5_o = x_5_o ^ beta_8_data_o;
+                x_6_o = x_6_o ^ beta_2_data_o;
+                x_7_o = x_7_o ^ beta_6_data_o;
+            `S4:
+                output_ready = 1'b0;
+                x_0_o = x_0_o ^ beta_4_data_o;
+                x_1_o = x_1_o ^ beta_8_data_o;
+                x_2_o = x_2_o ^ beta_6_data_o;
+                x_3_o = x_3_o ^ beta_1_data_o;
+                x_4_o = x_4_o ^ beta_9_data_o;
+                x_5_o = x_5_o ^ beta_12_data_o;
+                x_6_o = x_6_o ^ beta_3_data_o;
+                x_7_o = x_7_o ^ beta_2_data_o;            
+            `S5: next_state = `S6;
+                output_ready = 1'b0;
+                x_0_o = x_0_o ^ beta_2_data_o;
+                x_1_o = x_1_o ^ beta_4_data_o;
+                x_2_o = x_2_o ^ beta_3_data_o;
+                x_3_o = x_3_o ^ beta_8_data_o;
+                x_4_o = x_4_o ^ beta_12_data_o;
+                x_5_o = x_5_o ^ beta_6_data_o;
+                x_6_o = x_6_o ^ beta_9_data_o;
+                x_7_o = x_7_o ^ beta_1_data_o;     
+            `S6: 
+                output_ready = 1'b0;
+                x_0_o = x_0_o ^ beta_8_data_o;
+                x_1_o = x_1_o ^ beta_1_data_o;
+                x_2_o = x_2_o ^ beta_12_data_o;
+                x_3_o = x_3_o ^ beta_2_data_o;
+                x_4_o = x_4_o ^ beta_3_data_o;
+                x_5_o = x_5_o ^ beta_9_data_o;
+                x_6_o = x_6_o ^ beta_6_data_o;
+                x_7_o = x_7_o ^ beta_4_data_o;   
+            `S7: 
+                output_ready = 1'b0;
+                x_0_o = x_0_o ^ beta_9_data_o;
+                x_1_o = x_1_o ^ beta_3_data_o;
+                x_2_o = x_2_o ^ beta_8_data_o;
+                x_3_o = x_3_o ^ beta_6_data_o;
+                x_4_o = x_4_o ^ beta_2_data_o;
+                x_5_o = x_5_o ^ beta_1_data_o;
+                x_6_o = x_6_o ^ beta_4_data_o;
+                x_7_o = x_7_o ^ beta_12_data_o;
+            `DONE: output_ready = 1'b1;
+            default: next_state = `IDLE;
+        endcase
+    end
+
+endmodule
--- FPGA/verilog_files/ff256_lightweight_cos_transf/ff256_lightweight_cos_transf_defines.sv
+++ FPGA/verilog_files/ff256_lightweight_cos_transf/ff256_lightweight_cos_transf_defines.sv
@@ -1,8 +1,20 @@
 //=======================================================
-//  Title: Multiplier GF(256) Defines
+//  Title: Lghtweight cosine transform in GF(256) Defines
 //=======================================================
 
 `ifndef FF256_LIGHTWEIGHT_COS_TRANSF_DEFINES_V
 `define FF256_LIGHTWEIGHT_COS_TRANSF_DEFINES_V
+
+    // FSM defines
+    `define IDLE    4'b0000
+    `define S0      4'b0001
+    `define S1      4'b0010
+    `define S2      4'b0011
+    `define S3      4'b0100
+    `define S4      4'b0101
+    `define S5      4'b0110
+    `define S6      4'b0111
+    `define S7      4'b1000
+    `define DONE    4'b1001
 
 `endif
